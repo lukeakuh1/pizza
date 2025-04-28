04626ed9c21359c251998b8bdbbc004cd49397f5
luke <152388502+lukeakuh1@users.noreply.github.com>
2025-04-27 22:27:42 -0400

    Update akuh.py
    I create a greeting method in the Person class

--- akuh.py
+++ akuh.py
@@ -6,6 +6,10 @@ class Person:
     def __str__(self):
         return f"My Name is: {self.name} and i am {self.age} years old."
 
+    def greeting(self):
+        user = input("Enter You Name: ")
+        return f "Hello, {user} How are you doing today"
+
 
 def main():
     name = input("Enter your name: ")
@@ -13,6 +17,8 @@ def main():
 
     person = Person(name, age)
     print(person)
+    print()
+    print(person.greeting())
 
 
 if __name__ == "__main__":
